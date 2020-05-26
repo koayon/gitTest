689cf240caaf623cd63143ba08305ea3ca56473f
Kola <koayon@gmail.com>
2020-05-26 11:23:45 +0100

    Added Git commands

---
+++ script.py
@@ -0,0 +1,23 @@
+"""This is our new file"""
+
+#Consider committing
+
+"""More changes, more changes"""
+
+def count_for_me(x):
+    for i in range(x):
+        print(i)
+
+"""git add . <- adds everything in directory
+Commands:
+status
+commit
+add
+push
+pull
+
+Type git to return all the commands available
+
+to save when faced with commit write
+Esc  :wq
+"""
